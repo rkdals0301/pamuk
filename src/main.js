922a08e6dd5592129ba8e644439f25f932c08db1
이강민 <rkdals0301@naver.com>
2022-07-12 15:14:32 +0900

    pinia-settings

--- src/main.js
+++ src/main.js
@@ -3,12 +3,20 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import VueRouter from 'vue-router'
 import router from '@/router'
+import VueCompositionAPI from '@vue/composition-api'
+import { createPinia, PiniaVuePlugin } from 'pinia'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
+Vue.use(VueCompositionAPI)
+Vue.use(PiniaVuePlugin)
+
+
+const pinia = createPinia()
 
 new Vue({
   vuetify,
   router,
+  pinia,
   render: h => h(App)
 }).$mount('#app')
